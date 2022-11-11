44d96389b45bdc93d0934d7c24cbb8f6dc4349c0
Thomas Finneman <thomasfinneman94@gmail.com>
2022-11-11 16:09:12 +0100

    la till html för lista, js för viss logil

---
+++ script.js
@@ -0,0 +1,42 @@
+"use strict";
+/* vad ska vi göra här*/
+
+const bookList = [
+    {id: 1, author: "Charles Dickens", title: "Oliver Twist"},
+    {id: 2, author: "william Shakespear", title: "Hamlet"}
+];
+const searchInput = null;
+
+function handleKeyPress(input){
+    /*  ta emot/läsa av värdet i fältet i inputfältet
+        skicka värdet till searchBooks
+        searchBooks retunerar en filtrerad lista
+        filtrerade listan skickas till renderedBookList
+    */
+    searchBooks(input);
+}
+
+function searchBooks(searchTerm){
+    /*  loopa igenom bookList, och för varje varv i loopen, ta det aktuella elementet (boken)
+        jämnför titeln med söktermen
+        om söktärmen finns någonstans i titeln, lägg till element i ny listan(filteredList)
+        retunerar filteredList eller anroppar renderBookList
+    */
+    
+    const filteredList = [];
+    for(let i = 0; i < bookList.length; i++){
+        const title = bookList[i].title.toLowerCase();
+
+        if(title.indexOf(searchTerm.toLowerCase()) >= 0){
+            console.log("match?");
+            filteredList.push(bookList[0]);
+        }
+    }
+    renderBookList(filteredList); 
+}
+
+function renderBookList(list){
+    /* Elementet i HTML-listan visas/döljs beroende på listans innehåll. */
+    console.log(list)
+}
+handleKeyPress("e");
