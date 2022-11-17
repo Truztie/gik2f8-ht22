4c847fd7394c6d6be0f3729fd4cb5462dc8bfe82
Thomas Finneman <thomasfinneman94@gmail.com>
2022-11-17 15:52:53 +0100

    komponenter

--- script.js
+++ script.js
@@ -40,21 +40,13 @@ function searchBooks(searchTerm){
 
 function renderBookList(bookList){
     /* Elementet i HTML-listan visas/döljs beroende på listans innehåll. */
-    let html = `<ul class="book-list rounded-md border-2 border-blue-400 bg-white w-full mx-auto">`;
-    for(let i = 0; i < bookList.length; i++){
-        html += `<li class="book-list_item mb-2 mx-2 last:mb-0 p-3 text-indigo-900 last:border-b-0 border-b border-indigo-700 cursor-pointer">
-                ${bookList[i].author} - ${bookList[i].title}
-                </li>`
-    }
-    html += `</ul>`;
-
     const existingElement = document.querySelector(".book-list");
     console.log(existingElement);
     const root = document.getElementById("root");
     if(existingElement){
         root.removeChild(existingElement);
     }
-    const htmlElement = document.createElement("ul");
-    root.insertAdjacentHTML("beforeend", html);
-
+    if(bookList.length > 0){
+        root.insertAdjacentHTML("beforeend", BookList(bookList));
+    }
 }
