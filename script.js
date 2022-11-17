58a7b57d3f99d16e4400b2b2ab4a5d510bcdc48e
Thomas Finneman <thomasfinneman94@gmail.com>
2022-11-17 14:45:12 +0100

    lyssnar efter input

--- script.js
+++ script.js
@@ -5,15 +5,19 @@ const bookList = [
     {id: 1, author: "Charles Dickens", title: "Oliver Twist"},
     {id: 2, author: "william Shakespear", title: "Hamlet"}
 ];
-const searchInput = null;
+/*const searchField = document.children[0].children[1].children[1].children[1];*/
+const searchField = document.getElementById("searchField");
+console.log(searchField);
+/*keydown, keyup*/
+searchField.addEventListener("keyup", handleKeyPress);
 
-function handleKeyPress(input){
+function handleKeyPress(e){
     /*  ta emot/läsa av värdet i fältet i inputfältet
         skicka värdet till searchBooks
         searchBooks retunerar en filtrerad lista
         filtrerade listan skickas till renderedBookList
     */
-    searchBooks(input);
+    searchBooks(e.target.value);
 }
 
 function searchBooks(searchTerm){
@@ -39,4 +43,3 @@ function renderBookList(list){
     /* Elementet i HTML-listan visas/döljs beroende på listans innehåll. */
     console.log(list)
 }
-handleKeyPress("e");
